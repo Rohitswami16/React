2c7f5d1027b6551f573412c09492eef5d5d067ec
rohitswami16 <rohitswami1612@gmail.com>
2025-03-18 00:26:56 +0530

    react flow

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,12 @@
+import Chai from "./Chai";
+
 function App() {
-  
-
   return (
-    <h1>hello world from vite</h1>
+    <>
+      <Chai />
+      <h1>chai aur react</h1>
+    </>
   );
-
-  
 }
 
 export default App;
-
-
-
