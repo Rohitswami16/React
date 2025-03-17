ba3096a79aad265c3d99fe65bd170123b574a0b9
rohitswami16 <rohitswami1612@gmail.com>
2025-03-18 01:33:12 +0530

    done gn

--- customeReact/customreact.js
+++ customeReact/customreact.js
@@ -1,35 +1,31 @@
-function customRender(reactElement, conatainer) {
+function customRender(reactElement, container){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
 
-    // const domElement = document.createElement(reactElement.type)
-    // domElement.innerHTML = reactElement.children
-    // domElement.setAttribute('href', reactElement.props.href)
-    // domElement.setAttribute('target', reactElement.props.target)
-
-    // conatainer.appendChild(domElement)
-
+    container.appendChild(domElement)
+    */
 
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.innerHTML = reactElement.children
-
     for (const prop in reactElement.props) {
-        if (prop === 'children') continue
-        document.setAttribute(prop, reactElement.props[prop])
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
     }
-
-    conatainer.appendChild(domElement)
+    container.appendChild(domElement)
 }
 
 const reactElement = {
     type: 'a',
     props: {
-        href: 'http://google.com',
+        href: 'https://google.com',
         target: '_blank'
     },
-    children: "click me to visit google"
+    children: 'Click me to visit google'
 }
 
-const mainContainer = document.getElementById('root')
-
+const mainContainer = document.querySelector('#root')
 
 customRender(reactElement, mainContainer)
