40505cdd4502a5ffc9e2a06bfb32035cd0b7261b
rohitswami16 <rohitswami1612@gmail.com>
2025-03-18 00:55:27 +0530

    customReact

--- customeReact/customreact.js
+++ customeReact/customreact.js
@@ -1,3 +1,25 @@
+function customRender(reactElement, conatainer) {
+
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+
+    // conatainer.appendChild(domElement)
+
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.innerHTML = reactElement.children
+
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue
+        document.setAttribute(prop, reactElement.props[prop])
+    }
+
+    conatainer.appendChild(domElement)
+}
+
 const reactElement = {
     type: 'a',
     props: {
@@ -9,3 +31,5 @@ const reactElement = {
 
 const mainContainer = document.getElementById('root')
 
+
+customRender(reactElement, mainContainer)
