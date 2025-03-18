5911517e7cda61782e3d90b9d5dc018e6ebac9ef
rohitswami16 <rohitswami1612@gmail.com>
2025-03-18 14:11:09 +0530

    counter edge cases

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,31 +6,48 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let counter = 15;
+  const [counter, setCounter] = useState(15);
 
-  const count = document.getElementsByTagName("h2");
-  console.log(count);
+  const [msg, setMsg] = useState("");
+
+  // let counter = 15;
+  // const count = document.getElementsByTagName("h2");
+  // console.log(count);
 
   const addValue = () => {
-    counter += 1;
-    console.log(counter);
+    // counter += 1
+    // console.log(counter);
+    if (counter == 20) {
+      // alert("You have reached the maximum value");
+      setMsg("You have reached the maximum value");
+    } else {
+      setMsg("");
+      setCounter(counter + 1);
+    }
   };
 
   const removeValue = () => {
-    console.log(counter);
-    counter -= 1;
+    // console.log(counter);
+    // counter -= 1;
+    if (counter === 0) {
+      // alert("you have reached minimum value");
+      setMsg("You have reached the minimum value");
+    } else {
+      setMsg("");
+      setCounter(counter - 1);
+    }
   };
 
   return (
-    <>
-      <h1>Chai aur react</h1>
+    <div className="maincon">
+      <h1>Simple Counter</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
-      <br />
-      <br />
-      <br />
-      <button onClick={removeValue}>Remove Value</button>
-    </>
+      <div className="con">
+        <button onClick={addValue}>Add Value</button>
+        <button onClick={removeValue}>Remove Value</button>
+      </div>
+      <h2>{msg}</h2>
+      </div>
   );
 }
 
